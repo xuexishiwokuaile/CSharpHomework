0f49bf19166007474b81db0b50fc26dc72ead316
xuexishiwokuaile <37232631+xuexishiwokuaile@users.noreply.github.com>
2018-09-12 11:02:04 +0800

    Add files via upload

---
+++ TheFourthQuestion/ConsoleApp1/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入两个数：");
+            Console.Write("\n");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.Write("这两个数的积为：");
+            Console.Write("\n");
+            int k = i * j;
+            Console.Write(k);
+            Console.ReadKey();
+        }
+    }
+}
