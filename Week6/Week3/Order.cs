348763142719600648fbc62e78264d2485d54b8d
xuexishiwokuaile <37232631+xuexishiwokuaile@users.noreply.github.com>
2018-10-24 17:31:37 +0800

    Add files via upload

---
+++ Week6/Week3/Order.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6
+{
+    public class Order
+    {
+        private List<OrderDetail> details = new List<OrderDetail>();
+
+        public Order(uint orderId,Customer customer)
+        {
+            Id = orderId;
+            Customer = customer;
+        }
+
+        public uint Id { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public double Amount
+        {
+            get
+            {
+                return details.Sum(d => d.Goods.Price * d.Quantity);
+            }
+        }
+
+        public List<OrderDetail>Details
+        {
+            get => this.details;
+        }
+
+        public void AddDetails(OrderDetail orderDetail)
+        {
+            if(this.Details.Contains(orderDetail))
+            {
+                throw new Exception($"orderDetails-{orderDetail.Id} is already existed!");
+            }
+            details.Add(orderDetail);
+        }
+
+        public void RemoveDetails(uint orderDetailId)
+        {
+            details.RemoveAll(d => d.Id == orderDetailId);
+        }
+
+        public override string ToString()
+        {
+            string result = "================================================================================\n";
+            result += $"orderId:{Id}, customer:({Customer.Name}),Amount:{Amount}";
+            details.ForEach(od => result += "\n\t" + od);
+            result += "\n================================================================================";
+            return result;
+        }
+    }
+}
---
+++ Week6/Week3/OrderDetail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week6
+{
+    public class OrderDetail
+    {
+        public OrderDetail(uint id,Goods goods,uint quantity)
+        {
+            this.Id = id;
+            this.Goods = goods;
+            this.Quantity = quantity;
+        }
+
+        public uint Id { get; set; }
+
+        public Goods Goods { get; set; }
+
+        public uint Quantity { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var detail = obj as OrderDetail;
+            return detail != null &&
+                Goods.Id == detail.Goods.Id &&
+                Quantity == detail.Quantity;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1522631281;
+            hashCode = hashCode * -1521134295 + Goods.Name.GetHashCode();
+            hashCode = hashCode * -1521134295 + Quantity.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            result += $"orderDetailId:{Id}:  ";
+            result += Goods + $", quantity:{Quantity}";
+            return result;
+        }
+    }
+}
